957f1c291aea0ebb159428d5e9c8530a137e623e
Haejin <hj0816hj@gmail.com>
2021-09-08 17:04:00 +0900

    feat: add Application package, hello GET api

---
+++ src/main/java/example/org/spring_basic/web/HelloController.java
@@ -0,0 +1,12 @@
+package example.org.spring_basic.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}

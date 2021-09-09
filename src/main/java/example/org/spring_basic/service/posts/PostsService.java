28e970437d5abf6ff6a1bd67709a0798e697ad1f
Haejin <hj0816hj@gmail.com>
2021-09-10 00:41:38 +0900

    feat: add Post api

---
+++ src/main/java/example/org/spring_basic/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package example.org.spring_basic.service.posts;
+
+import example.org.spring_basic.domain.posts.PostsRepository;
+import example.org.spring_basic.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}

2a738d9b69ae7a767bda67bf9a21a4ea83bdd56e
Haejin <hj0816hj@gmail.com>
2021-10-04 22:46:49 +0900

    feat: add update, get api Service

--- src/main/java/example/org/spring_basic/service/posts/PostsService.java
+++ src/main/java/example/org/spring_basic/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package example.org.spring_basic.service.posts;
 
+import example.org.spring_basic.domain.posts.Posts;
 import example.org.spring_basic.domain.posts.PostsRepository;
+import example.org.spring_basic.web.dto.PostsResponseDto;
 import example.org.spring_basic.web.dto.PostsSaveRequestDto;
+import example.org.spring_basic.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
